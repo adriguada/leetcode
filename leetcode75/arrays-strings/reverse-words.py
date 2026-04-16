552c06a12a68293b2c3a6ce1d64d2d943bd1cbbb
guada <adrian.glez.guada@gmail.com>
2026-04-16 16:01:46 +0200

    exercise: Reverse words in a string

---
+++ leetcode75/arrays-strings/reverse-words.py
@@ -0,0 +1,13 @@
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        words = s.split()
+
+        i = 0
+        j = len(words)-1
+
+        while i < j:
+            words[i], words[j] = words[j], words[i]
+            i += 1
+            j -= 1
+
+        return " ".join(words)
