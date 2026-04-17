14c2eab93a51bfc5c6426fedbf4dc067b1a0a991
guada <adrian.glez.guada@gmail.com>
2026-04-17 20:31:54 +0200

    exercise: Container with most water

---
+++ leetcode75/two-pointers/container-with-most-water.py
@@ -0,0 +1,29 @@
+from typing import List
+
+"""
+Solution: Two-pointer approach.
+
+The idea is to start with the widest container and move the pointers inward,
+by moving the one pointing to the shorter line towards the center.
+"""
+
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        ans = 0
+
+        left = 0
+        right = len(height) - 1
+
+        while left < right:
+            x = right - left
+            y = min(height[left], height[right])
+            area = x * y
+            ans = max(ans, area)
+
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+
+        return ans
