def5b54a0d861a56bbdcc733394b2bb7bdd3197e
guada <adrian.glez.guada@gmail.com>
2026-04-17 21:51:14 +0200

    exercise: Max number of K-sum pairs

---
+++ leetcode75/two-pointers/max-num-of-k-sum-pairs.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+class Solution:
+    def maxOperations(self, nums: List[int], k: int) -> int:
+        ans = 0
+
+        left = 0
+        right = len(nums) - 1
+
+        nums.sort()
+
+        while left < right:
+            if nums[left] + nums[right] == k:
+                left += 1
+                right -= 1
+                ans += 1
+            elif nums[left] + nums[right] > k:
+                right -= 1
+            else:
+                left += 1
+
+        return ans
